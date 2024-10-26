53ffbcfff9d21b2495145dc4240de015a8cb25a6
BrunoMar99 <brunoemartinez99@gmail.com>
2024-10-26 12:15:35 -0300

    cambios en el controller de pago para poder armar el CRUD, falta el create ya que pide un pedido y todavia no esta desarrollado, es lo proximo a realizar, poder asignarle un pedido a un pago (deben ser pedidos que no esten pagados)

--- src/controllers/pagoController.ts
+++ src/controllers/pagoController.ts
@@ -1,81 +1,82 @@
 import { NextFunction, Request, Response } from 'express'
 import { Pago } from '../models/pago.entity.js'
 import { orm } from '../shared/db/orm.js'
+import { TipoPago } from '../models/tipoPago.entity.js'
 
 const em = orm.em
 
 async function findAll(req: Request, res: Response) {
   try {
-    const pago = await em.find(Pago, {}, { populate: ['tipoPago'] })
+    const pagos = await em.find(Pago, {})
     res
       .status(200)
-      .json({ message: 'Se encontraron los pagos!', data: pago })
+      .json({ pagos: pagos })
   } catch (error: any) {
     res.status(500).json({ message: error.message })
   }
 }
 
 async function add(req: Request, res: Response) {
+  console.log(req.body)
   try {
-    const pago = em.create(Pago, req.body)
-    const nuevaFecha = new Date()
-    pago.fecha = nuevaFecha
-    await em.flush()
-    res.status(201).json({ message: 'Pago creado!', data: pago })
+    const { tipoPago, ...pagoData } = req.body;
+    let tipoPagoEntity;
+
+    if (tipoPago?.id) {
+      tipoPagoEntity = await em.findOne(TipoPago, tipoPago.id);
+      if (!tipoPagoEntity) {
+        return res.status(404).json({ message: 'Tipo de Pago no encontrado' });
+      }
+    } else {
+      tipoPagoEntity = em.create(TipoPago, {
+        nombre: tipoPago.nombre,
+        descripcion: tipoPago.descripcion,
+      });
+    }
+
+    let pago = em.create(Pago, { ...pagoData, tipoPago: tipoPagoEntity });
+    await em.persistAndFlush(pago);
+    res.status(201).json({ message: 'Pago creado!', data: pago });
+  } catch (error: any) {
+    res.status(500).json({ message: error.message });
+  }
+
+}
+
+
+async function findOne(req: Request, res: Response) {
+  try {
+    const id = Number.parseInt(req.params.id)
+    const PagoEncontrado = await em.findOneOrFail(Pago, { id }, { populate: ['tipoPago'] })
+    res
+      .status(200)
+      .json({ message: 'se encontro el Pago!', data: PagoEncontrado })
   } catch (error: any) {
     res.status(500).json({ message: error.message })
   }
 }
 
 
-async function findOne(req: Request, res: Response) {
-  try {
-    const id = Number.parseInt(req.params.id);
-    const pagoEncontrado = await em.findOneOrFail(Pago, { id }, { populate: ['tipoPago'] });
-    res.status(200).json({ message: 'Se encontró el pago!', data: pagoEncontrado });
-  } catch (error: any) {
-    res.status(500).json({ message: error.message });
-  }
-}
-
-
 async function update(req: Request, res: Response) {
   try {
-    const id = Number.parseInt(req.params.id);
-
-    if (isNaN(id)) {
-      return res.status(400).json({ message: 'Invalid id parameter' });
-    }
-
-    const pagoToUpdate = await em.findOneOrFail(Pago, { id: id }, { populate: ['tipoPago'] });
-
-    em.assign(pagoToUpdate, req.body);
-    await em.flush();
-
-    res.status(200).json({ message: 'Pago actualizado!', data: pagoToUpdate });
+    const id = Number.parseInt(req.params.id)
+    const pagoToUpdate = await em.findOneOrFail(Pago, { id })
+    em.assign(pagoToUpdate, req.body)
+    await em.flush()
+    res.status(200).json({ message: 'Pago actualizado!', data: pagoToUpdate })
   } catch (error: any) {
-    if (error.name === 'EntityNotFoundError') {
-      return res.status(404).json({ message: 'Pago not found' });
-    }
-
-    res.status(500).json({ message: error.message });
+    res.status(500).json({ message: error.message })
   }
 }
 
 async function remove(req: Request, res: Response) {
   try {
-    const idPago = Number.parseInt(req.params.idPago, 10);
-
-    if (isNaN(idPago)) {
-      return res.status(400).json({ message: 'Invalid idPago parameter' });
-    }
-
-    // Delete by ID directly
-    await em.nativeDelete(Pago, idPago);
-
-    res.status(200).json({ message: 'Pago borrado!' });
+    const id = Number.parseInt(req.params.id)
+    const pago = await em.getReference(Pago, id)
+    await em.removeAndFlush(pago)
+    res.status(200).json({ message: 'Pago borrado!' })
   } catch (error: any) {
-    res.status(500).json({ message: error.message });
+    res.status(500).json({ message: error.message })
   }
 }
 
