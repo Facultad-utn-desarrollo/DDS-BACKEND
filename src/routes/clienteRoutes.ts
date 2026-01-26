64238095388c1ee4569bad1d0fbb449c031f4594
Fedeaieta <federicoaieta97@gmail.com>
2026-01-26 15:18:55 -0300

    Cambio en clietne routes para traer los activos

--- src/routes/clienteRoutes.ts
+++ src/routes/clienteRoutes.ts
@@ -1,12 +1,12 @@
 import { Router } from 'express';
 import { findAll, findOne, add, update, remove, findClientesActivos } from '../controllers/clienteController.js';
 
-export const clienteRouter = Router()
+export const clienteRouter = Router();
 
-clienteRouter.get('/', findAll)
-clienteRouter.get('/activos/', findClientesActivos)
-clienteRouter.get('/:id', findOne)
-clienteRouter.post('/', add)
-clienteRouter.put('/:id', update)
-clienteRouter.patch('/:id', update)
-clienteRouter.delete('/:id', remove)
+clienteRouter.get('/activos', findClientesActivos);
+clienteRouter.get('/', findAll);
+clienteRouter.get('/:id', findOne);
+clienteRouter.post('/', add);
+clienteRouter.put('/:id', update);
+clienteRouter.patch('/:id', update);
+clienteRouter.delete('/:id', remove);
