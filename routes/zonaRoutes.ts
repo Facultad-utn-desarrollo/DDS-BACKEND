8ca7f53f540128e94b06bd98e31873e987767a47
BrunoMar99 <brunoemartinez99@gmail.com>
2026-02-08 17:35:34 -0300

    * Corregi pq habia puesto la zona sin el middleware y no dejaba pasar ni al admin ni al cliente
    * no encontre archivos sin nada para borrar.
    
    * Lo unico que me quedo es la unificación de los idiomas en variables, funciones, etc. De ultima lo miro mañana antes de empezar con los testings y la ingesta de datos "reales".

--- routes/zonaRoutes.ts
+++ routes/zonaRoutes.ts
@@ -1,13 +1,14 @@
 import { Router } from 'express';
 import { findAll, findOne, add, update, remove, findZonasActivas } from '../controllers/zonaController.js';
 import { adminOnly } from '../middleware/adminOnly.js';
+import { authMiddleware } from '../middleware/auth.middleware.js';
 
 export const zonaRouter = Router()
 
 zonaRouter.get('/activos/', findZonasActivas)
-zonaRouter.get('/', adminOnly, findAll)
-zonaRouter.get('/:id', adminOnly, findOne)
-zonaRouter.post('/',  adminOnly,add)
-zonaRouter.put('/:id', adminOnly, update)
-zonaRouter.patch('/:id', adminOnly, update)
-zonaRouter.delete('/:id',  adminOnly, remove)
+zonaRouter.get('/', authMiddleware, adminOnly, findAll)
+zonaRouter.get('/:id', authMiddleware, adminOnly, findOne)
+zonaRouter.post('/', authMiddleware, adminOnly, add)
+zonaRouter.put('/:id', authMiddleware, adminOnly, update)
+zonaRouter.patch('/:id', authMiddleware, adminOnly, update)
+zonaRouter.delete('/:id', authMiddleware, adminOnly, remove)
